bc8fbdda933c0e416a5f0ee451e90c9dcf2fc539
fullop <parkth32@gmail.com>
2017-07-21 06:57:57 +0900

    item 2

--- Item2/CostEstimate.h
+++ Item2/CostEstimate.h
@@ -5,6 +5,6 @@ class CostEstimate
 	private:
 		static const double FudgeFactor; // 정적 클래스 상수의 선언, 헤더 파일에 존재해야 함
 	public:
-		const double getCost() const {return FudgeFactor;} ;
+		const double& getCost() const {return FudgeFactor;} ;
 };
 
--- Item2/GamePlayer.h
+++ Item2/GamePlayer.h
@@ -6,10 +6,10 @@ class GamePlayer
 		/*
 		 * static const int NumTurns = 5; // 선언 됨; 정의된 것이 아님
 		 */
-		enum {NumTurns = 5}; // 나열자 둔갑술(enum hack)
-		
+		enum {NumTurns = 5}; 	// 나열자 둔갑술(enum hack)
+					// enum이므로 reference type을 통한 접근이 불가능함 -> #define과 비슷한 효과!	
 		int scores[NumTurns];
 	public:
-		int getNum() { return NumTurns;};
+		const int getNum() const { return NumTurns;};
 
 };
